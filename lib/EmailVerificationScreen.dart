0db041d2626358c5c7c4aedbaf2f9203f5c0e464
Areeb Rajwana <areebrajwana@gmail.com>
2023-12-26 10:24:02 +0500

    update

--- lib/EmailVerificationScreen.dart
+++ lib/EmailVerificationScreen.dart
@@ -165,7 +165,7 @@ class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
     await FirebaseAuth.instance.verifyPhoneNumber(
       phoneNumber: '${widget.phoneNumber}',
       verificationCompleted: (PhoneAuthCredential credential) async {
-        await auth.signInWithCredential(credential);
+        // await auth.signInWithCredential(credential);
       },
       verificationFailed: (FirebaseAuthException e) async {
         if (e.code == 'invalid-phone-number') {
