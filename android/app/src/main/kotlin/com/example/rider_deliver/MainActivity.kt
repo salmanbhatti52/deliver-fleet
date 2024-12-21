eb793f3ed1abc9e82773305aa320cae105e42f62
Hammad Siddiqui <hammadraza5388@gmail.com>
2024-12-21 13:39:34 +0500

    add custom notification sound

--- android/app/src/main/kotlin/com/example/rider_deliver/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/rider_deliver/MainActivity.kt
@@ -1,6 +1,42 @@
 package com.deliverbygfl.partner
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.media.AudioAttributes
+import android.net.Uri
+import android.os.Build
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "booking_notifications"
+            val channelName = "Booking Notifications"
+            val soundFileName = "notif_tune" // File name without extension
+
+            val soundUri: Uri = Uri.parse("android.resource://${packageName}/raw/$soundFileName")
+
+            val attributes = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+
+            val channel = NotificationChannel(
+                    channelId,
+                    channelName,
+                    NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                setSound(soundUri, attributes) // Set custom sound
+                enableLights(true)
+                enableVibration(true)
+            }
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager?.createNotificationChannel(channel)
+        }
+    }
 }
